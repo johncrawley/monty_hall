5c01ddfe3463d97464a0d1ba6d8da768751be588
john <johncrawley1@gmail.com>
2023-05-17 21:55:54 +0100

    Replaced plain buttons with images for user choices.

--- app/src/main/java/com/jacstuff/montyhallproblem/MainActivity.java
+++ app/src/main/java/com/jacstuff/montyhallproblem/MainActivity.java
@@ -2,9 +2,11 @@ package com.jacstuff.montyhallproblem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Arrays;
@@ -13,9 +15,11 @@ import java.util.List;
 import java.util.Random;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    private Button choice1Button, choice2Button, choice3Button;
+    private View door1, door2, door3;
+    private View door1Border, door2Border, door3Border;
+    private Button playAgainButton;
     private TextView statisticsTextView;
     private TextView statusTextView;
     private Statistics statistics;
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private int prizeNumber = 1;
     private int selectedNumber = 0;
     private enum Mode { SELECT_CHOICE, CONFIRM_CHOICE, RESULT}
+    private List<View> doorBorders;
 
 
     @Override
@@ -32,59 +37,63 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         statistics = new Statistics();
         initViews();
         mode = Mode.SELECT_CHOICE;
-    }
 
+    }
 
     private void initViews(){
-        choice1Button = setupButton(R.id.choice1button, 1);
-        choice2Button =  setupButton(R.id.choice2button, 2);
-        choice3Button = setupButton( R.id.choice3button, 3);
-        Button playAgainButton = findViewById(R.id.new_game_button);
+
+        door1Border = findViewById(R.id.door1Border);
+        door2Border = findViewById(R.id.door2Border);
+        door3Border = findViewById(R.id.door3Border);
+        doorBorders = Arrays.asList(door1Border, door2Border, door3Border);
+        deselectAllBorders();
+
+        door1 = setupDoor(R.id.door1, door1Border, 1);
+        door2 = setupDoor(R.id.door2, door2Border, 2);
+        door3 = setupDoor(R.id.door3, door3Border, 3);
+
+
+
+        playAgainButton = findViewById(R.id.new_game_button);
+        playAgainButton.setOnClickListener(v -> startNewGame());
         Button resetStatsButton = findViewById(R.id.reset_stats_button);
+        resetStatsButton.setOnClickListener(v -> resetStats());
         statisticsTextView = findViewById(R.id.statistics_text);
         statusTextView = findViewById(R.id.statusText);
-        setOnClickListeners();
-        setOnClickListenerFor(playAgainButton, resetStatsButton);
     }
 
 
-    private void setOnClickListeners(){
-        choice1Button.setOnClickListener(this::selectChoice);
-        choice2Button.setOnClickListener(this::selectChoice);
-        choice3Button.setOnClickListener(this::selectChoice);
+    private void deselectAllBorders(){
+        doorBorders.forEach(v -> v.setBackgroundColor(getColor(R.color.backgroundColor)));
     }
 
 
-    private Button setupButton(int id, int tagValue){
-        Button button = findViewById(id);
-        button.setTag(tagValue);
-        return button;
+    private View setupDoor(int doorId, View doorBorder, int tag){
+        ImageView view = findViewById(doorId);
+        view.setTag(tag);
+        view.setOnClickListener(v -> onClickDoor(view, doorBorder));
+        return view;
     }
 
 
-    private void setOnClickListenerFor(View ... views){
-        for(View v : views){
-            v.setOnClickListener(this);
-        }
+    private void onClickDoor(ImageView doorView, View borderView){
+        setSelected(borderView);
+        selectChoice(doorView);
     }
 
 
-    public void onClick(View v){
-        int id = v.getId();
-      if(id == R.id.new_game_button) {
-            startNewGame();
-        }
-        else if(id == R.id.reset_stats_button){
-            resetStats();
-        }
+    private void setSelected(View borderView){
+        deselectAllBorders();
+        borderView.setBackgroundColor(Color.BLUE);
     }
 
 
     private void startNewGame(){
         assignPrizeIndex();
-        resetChoiceButtons();
+        resetDoors();
         resetStatusText();
         mode = Mode.SELECT_CHOICE;
+        playAgainButton.setVisibility(View.INVISIBLE);
     }
 
 
@@ -100,49 +109,79 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     private void selectChoice(View view){
-        Button button = (Button) view;
         if( mode == Mode.SELECT_CHOICE){
-            if(button.isEnabled()){
-                selectedNumber = (int) button.getTag();
+            if(view.isEnabled()){
+                selectedNumber = (int) view.getTag();
                 mode = Mode.CONFIRM_CHOICE;
                 displayConfirmChoiceText();
                 reduceChoices();
             }
         }
         else if( mode == Mode.CONFIRM_CHOICE){
-            selectedNumber = (int)button.getTag();
+            selectedNumber = (int)view.getTag();
+            if(prizeNumber == (int)view.getTag()){
+                changeDoorToPrize(view);
+            }
+            else{
+                changeDoorToFail(view);
+            }
             mode = Mode.RESULT;
             calculateAndDisplayResult();
+            playAgainButton.setVisibility(View.VISIBLE);
         }
+    }
+
+
+    private void changeDoorToPrize(View view){
+        ImageView iv = (ImageView)view;
+        iv.setImageResource(R.drawable.door_car);
+    }
+
+
+    private void changeDoorToFail(View view){
+        ImageView iv = (ImageView)view;
+        iv.setImageResource(R.drawable.door_goat);
     }
 
 
     private void reduceChoices(){
-        List<Button> choices = new LinkedList<>(Arrays.asList(choice1Button, choice2Button, choice3Button));
+        List<View> doors = new LinkedList<>(Arrays.asList(door1, door2, door3));
+
+        removeDoorSelectedByUserFrom(doors);
+
+        if(selectedNumber == prizeNumber){
+            openOneOfTheOtherDoors(doors);
+            return;
+        }
+        disableViewsThatDoNotContainPrize(doors);
+    }
+
+
+    private void openOneOfTheOtherDoors(List<View> doors){
+        int viewIndexToDisable = new Random(System.currentTimeMillis()).nextInt(2);
+        View doorToOpen = doors.get(viewIndexToDisable);
+        doorToOpen.setEnabled(false);
+        changeDoorToFail(doorToOpen);
+    }
+
+
+    private void removeDoorSelectedByUserFrom(List<View> choices){
         for(int i =0; i< choices.size(); i++){
-            Button button = choices.get(i);
-            if((int)button.getTag() == selectedNumber){
+            if((int)choices.get(i).getTag() == selectedNumber){
                 choices.remove(i);
                 break;
             }
         }
-        if(selectedNumber == prizeNumber){
-            int buttonIndexToDisable = new Random(System.currentTimeMillis()).nextInt(2);
-            Button button = choices.get(buttonIndexToDisable);
-            button.setEnabled(false);
-            return;
-        }
-        disableButtonsThatDontContainPrize(choices);
     }
 
 
-    private void disableButtonsThatDontContainPrize(List<Button> buttonList){
-        for(int i = 0; i< buttonList.size(); i++){
-            Button button = buttonList.get(i);
-            if((int)button.getTag() != prizeNumber){
-                button.setEnabled(false);
+    private void disableViewsThatDoNotContainPrize(List<View> views){
+        views.forEach((v)->{
+            if((int)v.getTag() != prizeNumber){
+                v.setEnabled(false);
+                changeDoorToFail(v);
             }
-        }
+        } );
     }
 
 
@@ -185,16 +224,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    private void resetChoiceButtons(){
-        reset(choice1Button);
-        reset(choice2Button);
-        reset(choice3Button);
+    private void resetDoors(){
+        deselectAllBorders();
+        reset(door1);
+        reset(door2);
+        reset(door3);
     }
 
 
-    private void reset(Button button){
-        button.setEnabled(true);
-        button.setText(R.string.question_mark);
+    private void reset(View view){
+        view.setEnabled(true);
+        ImageView iv = (ImageView)view;
+        iv.setImageResource(R.drawable.door);
     }
 
 
--- app/src/main/java/com/jacstuff/montyhallproblem/Statistics.java
+++ app/src/main/java/com/jacstuff/montyhallproblem/Statistics.java
@@ -4,11 +4,35 @@ public class Statistics {
 
 
     private int gamesPlayed, gamesWon, gamesLost;
+    private int gamesPlayedWithKeepChoice, gamesWonWithKeepChoice;
+    private int gamesPlayedWithSwitchChoice, gamesWonWithSwitchChoice;
 
     void addGameWon(){
         gamesPlayed++;
         gamesWon++;
     }
+
+    void addGameWon(boolean wasChoiceKept){
+        gamesPlayed++;
+        if(wasChoiceKept){
+            gamesPlayedWithKeepChoice++;
+            gamesWonWithKeepChoice++;
+            return;
+        }
+        gamesPlayedWithSwitchChoice++;
+        gamesWonWithSwitchChoice++;
+    }
+
+
+    void addGameLost(boolean wasChoiceKept){
+        gamesPlayed++;
+        if(wasChoiceKept){
+            gamesPlayedWithKeepChoice++;
+            return;
+        }
+        gamesPlayedWithSwitchChoice++;
+    }
+
 
     void addGameLost(){
         gamesPlayed++;
@@ -19,6 +43,10 @@ public class Statistics {
         gamesPlayed = 0;
         gamesWon = 0;
         gamesLost = 0;
+        gamesPlayedWithKeepChoice = 0;
+        gamesWonWithKeepChoice = 0;
+        gamesPlayedWithSwitchChoice = 0;
+        gamesWonWithSwitchChoice = 0;
     }
 
 
@@ -28,6 +56,15 @@ public class Statistics {
 
     int getGamesWon(){
         return gamesWon;
+    }
+
+    String getKeepChoiceStats(){
+        return gamesWonWithKeepChoice + " / " + gamesPlayedWithKeepChoice;
+    }
+
+
+    String getSwitchChoiceStats(){
+        return gamesWonWithSwitchChoice + " / " + gamesPlayedWithSwitchChoice;
     }
 
     int getGamesLost(){
